208f5b32c967d811586e1e933605d5318067b809
oliveira-victor <victor.freire.oliveira@gmail.com>
2023-11-29 14:10:37 -0300

    update: new server address at Vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import { Game } from '../App'
 
 const api = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://digital-dice-data.onrender.com/'
+        baseUrl: 'https://api-storage-digitaldice.vercel.app/'
     }),
     endpoints: builder => ({
         getGames: builder.query<Game[], void>({
@@ -15,6 +15,3 @@ const api = createApi({
 export const { useGetGamesQuery } = api
 
 export default api
-
-  /* 'http://localhost:4000/products' */
-/*   https://github.com/oliveira-victor/servidor_estaticos/blob/main/digital-dice_server/db.json */
