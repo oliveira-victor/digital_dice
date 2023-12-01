94173498d2a3ddfcb51423a8a316b734e7015152
oliveira-victor <victor.freire.oliveira@gmail.com>
2023-11-30 22:16:14 -0300

    style: changing color of phone menu elements to main color

--- src/components/PhoneMenu/styles.ts
+++ src/components/PhoneMenu/styles.ts
@@ -1,4 +1,5 @@
 import styled, { keyframes } from "styled-components";
+import variables from "../../styles/variables";
 
 const bounce = keyframes`
     0%, 20%, 50%, 80%, 100% {transform: translate(8px, -8px);}
@@ -31,7 +32,7 @@ export const PhoneMenuButton = styled.li`
     width: 60px;
     height: 100%;
     text-align: center;
-    color: #8c8c8c;
+    color: ${variables.mainColor};
 `
 
 export const BtnIcon = styled.img`
