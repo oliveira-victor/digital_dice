871fa7432a9e7d86c70f90c8eb3787069974ecda
oliveira-victor <victor.freire.oliveira@gmail.com>
2023-12-14 16:08:39 -0300

    feat: new hero elements

--- src/containers/Hero/index.tsx
+++ src/containers/Hero/index.tsx
@@ -1,9 +1,20 @@
+import { ContentWrapper } from '../../styles'
 import * as S from './styles'
 
 const Hero = () => {
     return (
         <S.HeroImg>
             <S.HeroGradient></S.HeroGradient>
+            <ContentWrapper>
+                <div>
+                    <S.ReleaseTitle>Our latest release</S.ReleaseTitle>
+                    <S.HeroGameTitle>The Jungle Quest</S.HeroGameTitle>
+                    <S.HeroText>
+                        Help Iara save her people in this new incredible release <br />from <span>$ 23</span> to only $15 for a limited time!
+                    </S.HeroText>
+                    <S.HeroBtn>Get it now!</S.HeroBtn>
+                </div>
+            </ContentWrapper>
         </S.HeroImg>
     )
 }
--- src/containers/Hero/styles.ts
+++ src/containers/Hero/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import heroImg from '../../assets/hero.webp'
+import variables from "../../styles/variables";
 
 export const HeroImg = styled.section`
     background-image: url(${heroImg});
@@ -9,10 +10,25 @@ export const HeroImg = styled.section`
     height: 80vh;
     background-attachment: fixed;
     position: relative;
+    text-align: right;
+
+    div {
+        padding-top: 40px;
+        text-shadow: 0px 10px 10px rgba(0, 0, 0, 0.9);
+
+        @media (max-width: 767px) {
+            padding-top: 20px;
+
+            button {
+                font-size: 20px;
+            }
+        }
+    }
 
     @media (max-width: 767px) {
         height: 70vh;
         background-position: -140px;
+        text-align: center;
     }
 `
 
@@ -23,4 +39,60 @@ export const HeroGradient = styled.div`
     bottom: 0;
     background: rgb(${(props) => props.theme.gradientBg});
     background: linear-gradient(0deg, rgba(${(props) => props.theme.gradientBg},1) 0%, rgba(${(props) => props.theme.gradientBg},0) 100%);
+`
+
+export const ReleaseTitle = styled.button`
+    font-size: 24px;
+    border: none;
+    border-radius: 6px;
+    padding: 10px 20px;
+    background-color: ${variables.secondColor};
+    color: #fff;
+`
+
+export const HeroGameTitle = styled.span`
+    font-size: 84px;
+    color: #fff;
+    display: block;
+    margin-top: 20px;
+
+    @media (max-width: 767px) {
+        font-size: 40px;
+    }
+`
+
+export const HeroText = styled.p`
+    font-size: 28px;
+    color: #fff;
+    margin-top: 120px;
+
+    span {
+        text-decoration: line-through;
+    }
+
+    @media (max-width: 767px) {
+        font-size: 16px;
+        margin-top: 20px;
+    }
+`
+
+export const HeroBtn = styled.button`
+    background-color: ${variables.mainColor};
+    color: #fff;
+    font-size: 32px;
+    margin-top: 20px;
+    padding: 10px 30px;
+    border-radius: 6px;
+    border: none;
+    cursor: pointer;
+    transition: all .5s ease;
+
+    &:hover {
+        transform: translateY(-4px);
+        background-color: ${variables.addBtnHover};
+    }
+
+    @media (max-width: 767px) {
+        margin-top: 60px;
+    }
 `
